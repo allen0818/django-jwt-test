ee693cec1a60c3d875537add1b8a5487b1b2b144
allen0818 <chshman310222@gmail.com>
2020-03-15 22:56:13 +0800

    include more fields in jwt-token response

--- user/serializers.py
+++ user/serializers.py
@@ -13,10 +13,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+    # @classmethod
+    # def get_token(cls, user):
+    #     token = super().get_token(user)
 
-        # add custom claims
-        token['name'] = 'extra_info' # not working
-        return token
+    #     # add custom claims
+    #     token['name'] = 'extra_info' # not working
+    #     return token
+
+    # ref: https://stackoverflow.com/questions/53480770
+    def validate(self, attrs):
+        data = super(MyTokenObtainPairSerializer, self).validate(attrs)
+        data.update({'name': self.user.username})
+        data.update({'info': 'some extra infos'})
+        return data
